4eb3b7cf6226995ca790c7bc2ad8fd047bc0e691
Your Namecomposer <antoine.ducolomb@etu.univ-lyon1.fr>
2024-09-16 12:26:58 +0200

    affichage du top 50 bon avec photo

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -1,18 +1,39 @@
 <?php
+// src/Controller/SongController.php
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 class SongController extends AbstractController
 {
-    #[Route('/song', name: 'app_song')]
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    #[Route('/song', name: 'song_list')]
     public function index(): Response
     {
+        $accessToken = 'BQApHuCuCNSbi3hu0bcMaKTgxQCtb1G2sopksVa0yUOMtW0m_l_Flu3GgpsPFXZ_pDspabVln9Uzqw0iDmPr24bFmSKylzYD3IRQC8WoMK0Ybz118v0';
+
+        $url = 'https://api.spotify.com/v1/playlists/37i9dQZEVXbIPWwFssbupI?si=df6a34b8edf44353';
+
+        $response = $this->client->request('GET', $url, [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $accessToken,
+            ],
+        ]);
+
+        $playlistData = $response->toArray();
+
         return $this->render('song/index.html.twig', [
-            'controller_name' => 'SongController',
+            'playlist' => $playlistData,
         ]);
     }
 }
